bc2af6e16c8d9811f09fc8e303094fea6d1d95f3
Ratchanon Panmas <qq.panmas@gmail.com>
2023-03-16 00:17:28 +0700

    loading skeleton responsive

--- src/components/partial/GameCard/GameCardSkeleton.tsx
+++ src/components/partial/GameCard/GameCardSkeleton.tsx
@@ -1,118 +1,30 @@
-import { Box, HStack, Skeleton, Text, VStack } from "@chakra-ui/react";
+import { Flex, HStack, Skeleton, SkeletonText, VStack } from "@chakra-ui/react";
 
 type Props = {};
 
 export default function GameCardSkeleton({ }: Props) {
 
   return (
-    <HStack
+    <VStack
       w="100%"
       borderRadius={10}
-      px={{
-        base: 5,
-        md: 10,
-      }}
-      py={{
-        base: 5,
-        md: 5,
-      }}
+      p={5}
       border="2px"
       borderColor="gray.100"
       bg="white"
+      alignItems="start"
     >
-      <HStack
-        flex={1}
-        spacing={{
-          base: 5,
-          md: 16,
-        }}
-        textAlign="left"
-      >
-        <VStack alignItems="left" spacing={0}>
-          <Text
-            textColor="gray.400"
-            fontSize="sm"
-            fontFamily="athiti"
-            fontWeight="500"
-          >
-            เวลา
-          </Text>
-
-          <Skeleton w={14}
-            h={{
-              base: "24px",
-              md: "40px",
-            }}
-            startColor="pink.200"
-            endColor="pink.100" />
-
-        </VStack>
-
-        <VStack spacing={0} alignItems="left">
-          <Text
-            textColor="gray.400"
-            fontSize="sm"
-            fontFamily="athiti"
-            fontWeight="500"
-          >
-            กีฬา
-          </Text>
-          <Text
-            fontFamily="athiti"
-            fontSize={{ base: "md", md: "2xl" }}
-            fontWeight="bold"
-          >
-            <Skeleton w={20} h={{
-              base: "24px",
-              md: "40px",
-            }} />
-          </Text>
-        </VStack>
-        <VStack spacing={0} alignItems="left">
-          <Text
-            textColor="gray.400"
-            fontSize="sm"
-            fontFamily="athiti"
-            fontWeight="500"
-          >
-            ประเภท
-          </Text>
-          <Text
-            fontFamily="athiti"
-            fontSize={{ base: "md", md: "2xl" }}
-            fontWeight="bold"
-          >
-            <Skeleton w={20} h={{
-              base: "24px",
-              md: "40px",
-            }} />
-          </Text>
-        </VStack>
-        <VStack spacing={0} alignItems="left">
-          <Text
-            textColor="gray.400"
-            fontSize="sm"
-            fontFamily="athiti"
-            fontWeight="500"
-          >
-            สถานที่
-          </Text>
-          <Text
-            fontFamily="athiti"
-            fontSize={{ base: "md", md: "2xl" }}
-            fontWeight="bold"
-          >
-            <Skeleton w={20} h={{
-              base: "24px",
-              md: "40px",
-            }} />
-          </Text>
-        </VStack>
-
-        <Box display={{ base: "none", md: "block" }}>
-          <Skeleton w={20} h="30px" />
-        </Box>
+      <HStack>
+        <Skeleton w={["80px", "105px"]} h={["18px", "24px"]} />
+        <Skeleton w={["100px", "125px"]} h={["18px", "24px"]} />
       </HStack>
-    </HStack>
+      <Flex w="full" alignItems="center">
+        <Skeleton w="80px" h="45px" startColor="pink.300" endColor="pink.100" display={["none", "block"]} />
+        <Skeleton ml={[0, 4]} w={["80px", "105px"]} h="45px" />
+        <Skeleton ml={[2, 24]} w={["100px", "125px"]} h="45px" />
+        <Skeleton ml="auto" w="45px" h="24px" startColor="pink.300" endColor="pink.100" display={["block", "none"]} />
+      </Flex>
+      <SkeletonText noOfLines={1} w={100} />
+    </VStack>
   );
 }
